22760477e19d441eb47e0fea97898acfcb415848
Rohit Chandwani <rohit.chandwani@UE-B-NOTE-043.local>
2020-03-12 10:55:19 +0530

    Add test.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User, auth
 
 # Create your tests here.
+class LoginTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test1234', email='test@gmail.com')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = auth.authenticate(username='test', password='test1234')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = auth.authenticate(username='test1', password='test1234')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_pssword(self):
+        user = auth.authenticate(username='test', password='test123')
+        self.assertFalse(user is not None and user.is_authenticated)
